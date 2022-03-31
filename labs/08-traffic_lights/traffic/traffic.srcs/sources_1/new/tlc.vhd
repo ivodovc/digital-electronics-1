02baf842d865579abcf22f377ca8519b3c512184
student <student@PC-632.urel.feec.vutbr.cz>
2022-03-31 12:13:37 +0200

    funkcna verzia

--- labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/tlc.vhd
+++ labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/tlc.vhd
@@ -74,11 +74,11 @@ begin
     -- the frequency of the clock signal is 100 MHz.
     
     -- USE THIS PART FOR FASTER/SHORTER SIMULATION
-    s_en <= '1';
+    
     --USE THE FOLLOWING PART FOR THE IMPLEMENTATION
     clk_en0 : entity work.clock_enable
         generic map(
-            g_MAX => 10 --25000000 -- 250 ms / (1/100 MHz)
+            g_MAX => 25000000 -- 250 ms / (1/100 MHz)
         )
         port map(
             clk   => clk,
